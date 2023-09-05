d58dde49a579582d03f8991272ac1004123dd5a1
Peppssi <jesper2follin@gmail.com>
2023-09-05 21:11:00 +0200

    h

--- clock/src/ClockMain.java
+++ clock/src/ClockMain.java
@@ -6,65 +6,124 @@ import clock.io.ClockInput;
 import clock.io.ClockInput.UserInput;
 import clock.io.ClockOutput;
 
-// class Monitor {
+class Monitor {
 
-//     public synchronized void clockHandler() throws InterruptedException {
+    private int timeSec = 0;
+    private int timeMin = 0;
+    private int timeHour = 0;
 
-//         while (true) {
-//             Thread.sleep(1000);
-//             System.out.println("clockHandler Test");
-//         }
-//     }
+    private int alarmSec = 0;
+    private int alarmMin = 0;
+    private int alarmHour = 0;
 
-//     public synchronized void alarmHandler() throws InterruptedException {
+    private boolean indState = false;
 
-//         while (true) {
-//             Thread.sleep(1000);
-//             System.out.println("alarmHandler Test");
-//         }
-//     }
+    private Semaphore mutex = new Semaphore(1);
 
-// }
+    private long startTime = System.currentTimeMillis();
+
+    public void clockTicking(ClockOutput out) throws InterruptedException {
+
+        while (true) {
+            out.displayTime(timeHour, timeMin, timeSec);
+
+            if (timeHour == alarmHour && timeMin == alarmMin && timeSec == alarmSec && indState) {
+                out.alarm();
+            }
+
+            long currentTime = System.currentTimeMillis();
+            long elapsedTime = currentTime - startTime;
+            long nextExecutionTime = (timeSec + timeMin * 60 + timeHour * 60 * 60 + 1) * 1000;
+            long sleepTime = nextExecutionTime - elapsedTime;
+            if (sleepTime > 0) {
+                Thread.sleep(sleepTime);
+            }
+
+            mutex.acquire();
+            timeSec++;
+            mutex.release();
+
+            if (timeSec == 60) {
+                timeSec = 0;
+                timeMin++;
+                if (timeMin == 60) {
+                    timeMin = 0;
+                    timeHour++;
+                }
+                if (timeHour == 24) {
+                    timeHour = 0;
+                }
+            }
+        }
+    }
+
+    public void setTime(int hour, int min, int sec) throws InterruptedException {
+
+        mutex.acquire();
+        timeHour = hour;
+        timeMin = min;
+        timeSec = sec;
+        mutex.release();
+
+    }
+
+    public void setAlarm(int hour, int min, int sec) throws InterruptedException {
+        mutex.acquire();
+        alarmHour = hour;
+        alarmMin = min;
+        alarmSec = sec;
+        mutex.release();
+    }
+
+    public void toggleAlarm(ClockOutput out) throws InterruptedException {
+        mutex.acquire();
+        out.setAlarmIndicator(!indState);
+        indState = !indState;
+        mutex.release();
+    }
+
+}
 
 public class ClockMain {
     public static void main(String[] args) throws InterruptedException {
         AlarmClockEmulator emulator = new AlarmClockEmulator();
-        //Monitor mon = new Monitor();
-        //Semaphore mutex = new Semaphore(1);
+        Monitor mon = new Monitor();
 
         ClockInput in = emulator.getInput();
         ClockOutput out = emulator.getOutput();
+        Semaphore sem = in.getSemaphore();
 
-        // Thread clockThread = new Thread(() -> {
-        //     try {
-        //         mon.clockHandler();
-        //     } catch (InterruptedException e) {
-        //         System.out.println("InterruptedException hände i Clockhandler");
-        //         e.printStackTrace();
-        //     }
-        // });
-        // clockThread.start();
+        Thread clockThread = new Thread(() -> {
+            try {
+                mon.clockTicking(out);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
 
-        // Thread alarmThread = new Thread(() -> {
-        //     try {
-        //         mon.alarmHandler();
-        //     } catch (InterruptedException e) {
-        //         System.out.println("InterruptedException hände i alarmhandler");
-        //         e.printStackTrace();
-        //     }
-        // });
-        // alarmThread.start();
-
-        out.displayTime(15, 2, 37); // arbitrary time: just an example
+        clockThread.start();
 
         while (true) {
+            sem.acquire();
             UserInput userInput = in.getUserInput();
             Choice c = userInput.choice();
             int h = userInput.hours();
             int m = userInput.minutes();
             int s = userInput.seconds();
             System.out.println("choice=" + c + " h=" + h + " m=" + m + " s=" + s);
-            //mutex.release();
+            switch (c.toString()) {
+                case "SET_TIME":
+                    mon.setTime(h, m, s);
+                    break;
+
+                case "SET_ALARM":
+                    mon.setAlarm(h, m, s);
+                    break;
+
+                case "TOGGLE_ALARM":
+                    mon.toggleAlarm(out);
+                    break;
+            }
         }
     }
 

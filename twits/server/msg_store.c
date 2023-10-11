40fccaccf27ae82a9d2e3e6519b45042ab89bbb7
Peppssi <jesper2follin@gmail.com>
2023-10-11 11:35:10 +0200

    s

--- twits/server/msg_store.c
+++ twits/server/msg_store.c
@@ -8,14 +8,16 @@
 #include "list.h"
 #include "msg_store.h"
 
-struct msg_store {
+struct msg_store
+{
   // A list of topics. Each element points to another list,
   // holding the messages in that topic.
   struct list *topics;
 };
 
 // one list item, representing a message including metadata
-struct message {
+struct message
+{
   char *username;
   char *text;
 };
@@ -37,9 +39,11 @@ static bool
 any_message_available(struct msg_store *store,
                       struct client_state *client)
 {
-  for (int i = 0; i < list_size(client->message_counts); i++) {
+  for (int i = 0; i < list_size(client->message_counts); i++)
+  {
     struct list *messages_in_topic = list_get(store->topics, i);
-    if (list_size(messages_in_topic) > list_get_int(client->message_counts, i)) {
+    if (list_size(messages_in_topic) > list_get_int(client->message_counts, i))
+    {
       return true;
     }
   }
@@ -53,8 +57,7 @@ any_message_available(struct msg_store *store,
 static bool
 client_is_logging_out(struct client_state *client)
 {
-  return (client->current_topic_id == TOPIC_STATE_LOGOUT_REQUESTED
-          || client->current_topic_id == TOPIC_STATE_DISCONNECTED);
+  return (client->current_topic_id == TOPIC_STATE_LOGOUT_REQUESTED || client->current_topic_id == TOPIC_STATE_DISCONNECTED);
 }
 
 // ============================================================================
@@ -71,10 +74,9 @@ msg_store_create()
 
 // ----------------------------------------------------------------------------
 
-int
-msg_store_add_topic(struct msg_store *store,
-                    char *username,
-                    char *text)
+int msg_store_add_topic(struct msg_store *store,
+                        char *username,
+                        char *text)
 {
   struct message *m = malloc(sizeof(struct message));
   m->username = strdup(username);
@@ -90,11 +92,10 @@ msg_store_add_topic(struct msg_store *store,
 
 // ----------------------------------------------------------------------------
 
-void
-msg_store_add_message(struct msg_store *store,
-                      struct client_state *client,
-                      char *username,
-                      char *text)
+void msg_store_add_message(struct msg_store *store,
+                           struct client_state *client,
+                           char *username,
+                           char *text)
 {
   struct message *m = malloc(sizeof(struct message));
   m->username = strdup(username);
@@ -106,15 +107,15 @@ msg_store_add_message(struct msg_store *store,
 
 // ----------------------------------------------------------------------------
 
-bool
-msg_store_check_for_new_topic(struct msg_store *store,
-                              struct client_state *client,
-                              int *topic_id,
-                              char **username,
-                              char **text)
+bool msg_store_check_for_new_topic(struct msg_store *store,
+                                   struct client_state *client,
+                                   int *topic_id,
+                                   char **username,
+                                   char **text)
 {
   bool available = any_topic_available(store, client);
-  if (available) {
+  if (available)
+  {
     list_add_int(client->message_counts, 0);
     int last_topic_published = list_size(client->message_counts) - 1;
     *topic_id = last_topic_published;
@@ -130,21 +131,23 @@ msg_store_check_for_new_topic(struct msg_store *store,
 
 // ----------------------------------------------------------------------------
 
-bool
-msg_store_check_for_new_message(struct msg_store *store,
-                                struct client_state *client,
-                                int *topic_id,
-                                int *message_id,
-                                char **username,
-                                char **text)
+bool msg_store_check_for_new_message(struct msg_store *store,
+                                     struct client_state *client,
+                                     int *topic_id,
+                                     int *message_id,
+                                     char **username,
+                                     char **text)
 {
-  bool available = (client->current_topic_id >= 0)
-                   && (client->current_topic_id < list_size(store->topics));
-  if (available) {
+  bool available = (client->current_topic_id >= 0) && (client->current_topic_id < list_size(store->topics));
+  if (available)
+  {
     struct list *topic = list_get(store->topics, client->current_topic_id);
-    if (client->nbr_read >= list_size(topic)) {
+    if (client->nbr_read >= list_size(topic))
+    {
       available = false;
-    } else {
+    }
+    else
+    {
       struct message *m = list_get(topic, client->nbr_read);
       *topic_id = client->current_topic_id;
       *message_id = client->nbr_read;
@@ -160,17 +163,18 @@ msg_store_check_for_new_message(struct msg_store *store,
 
 // ----------------------------------------------------------------------------
 
-bool
-msg_store_check_for_updated_message_count(struct msg_store *store,
-                                          struct client_state *client,
-                                          int *topic_id,
-                                          int *message_count)
+bool msg_store_check_for_updated_message_count(struct msg_store *store,
+                                               struct client_state *client,
+                                               int *topic_id,
+                                               int *message_count)
 {
   bool available = false;
-  for (int i = 0; i < list_size(client->message_counts); i++) {
+  for (int i = 0; i < list_size(client->message_counts); i++)
+  {
     struct list *messages = list_get(store->topics, i);
     int count = list_size(messages);
-    if (list_get_int(client->message_counts, i) != count) {
+    if (list_get_int(client->message_counts, i) != count)
+    {
       list_set_int(client->message_counts, i, count);
       *topic_id = i;
       *message_count = count;
@@ -184,13 +188,10 @@ msg_store_check_for_updated_message_count(struct msg_store *store,
 
 // ----------------------------------------------------------------------------
 
-int
-msg_store_await_message_or_topic(struct msg_store *store,
-                                 struct client_state *client)
+int msg_store_await_message_or_topic(struct msg_store *store,
+                                     struct client_state *client)
 {
-  while (! any_topic_available(store, client)
-      && ! any_message_available(store, client)
-      && ! client_is_logging_out(client))
+  while (!any_topic_available(store, client) && !any_message_available(store, client) && !client_is_logging_out(client))
   {
   }
 
@@ -201,9 +202,8 @@ msg_store_await_message_or_topic(struct msg_store *store,
 
 // ----------------------------------------------------------------------------
 
-void
-msg_store_init_client(struct msg_store *store,
-                      struct client_state *client)
+void msg_store_init_client(struct msg_store *store,
+                           struct client_state *client)
 {
   client->current_topic_id = TOPIC_STATE_NO_TOPIC;
   client->message_counts = list_create();
@@ -211,22 +211,21 @@ msg_store_init_client(struct msg_store *store,
 
 // ----------------------------------------------------------------------------
 
-void
-msg_store_dispose_client(struct msg_store *store,
-                         struct client_state *client)
+void msg_store_dispose_client(struct msg_store *store,
+                              struct client_state *client)
 {
   list_destroy(client->message_counts);
 }
 
 // ----------------------------------------------------------------------------
 
-void
-msg_store_select_topic(struct msg_store *store,
-                       struct client_state *client,
-                       int topic_id)
+void msg_store_select_topic(struct msg_store *store,
+                            struct client_state *client,
+                            int topic_id)
 {
   client->current_topic_id = topic_id;
-  if (topic_id >= 0) {
+  if (topic_id >= 0)
+  {
     list_set_int(client->message_counts, topic_id, 0);
   }
   client->nbr_read = 0;
